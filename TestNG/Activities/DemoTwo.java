c82f4e417ccca8a19d19afca1c65adfd7b03cee5
Gaurav Kumar20 <gkumar20@in.ibm.com>
2021-07-02 19:54:52 +0530

    added TestNG Activities

---
+++ TestNG/Activities/DemoTwo.java
@@ -0,0 +1,10 @@
+package TestNGActivities;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+    @Test
+    public void ZTestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
